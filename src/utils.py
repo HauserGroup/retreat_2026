8fa8c072203575ad40de48be09b5e910d594b1b5
jsture <jakobsturemadsen@gmail.com>
2026-04-16 21:21:25 +0200

    Kernel PCA and rdkit

--- src/utils.py
+++ src/utils.py
@@ -8,3 +8,33 @@ def data_path(*args: str | Path) -> Path:
     if not args:
         return PROJECT_ROOT / "data"
     return PROJECT_ROOT / "data" / Path(*args)
+
+
+Set1 = {
+    3: ["#e41a1c", "#377eb8", "#4daf4a"],
+    4: ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3"],
+    5: ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00"],
+    6: ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#ffff33"],
+    7: ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#ffff33", "#a65628"],
+    8: [
+        "#e41a1c",
+        "#377eb8",
+        "#4daf4a",
+        "#984ea3",
+        "#ff7f00",
+        "#ffff33",
+        "#a65628",
+        "#f781bf",
+    ],
+    9: [
+        "#e41a1c",
+        "#377eb8",
+        "#4daf4a",
+        "#984ea3",
+        "#ff7f00",
+        "#ffff33",
+        "#a65628",
+        "#f781bf",
+        "#999999",
+    ],
+}
